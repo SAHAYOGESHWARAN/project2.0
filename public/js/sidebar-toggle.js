a20725f228f182313d4877b0e188b49b4e81bcac
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-18 08:58:16 +0530

    2.0

---
+++ public/js/sidebar-toggle.js
@@ -0,0 +1,9 @@
+// sidebar-toggle.js
+const toggleButton = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('collapsed');
+});
