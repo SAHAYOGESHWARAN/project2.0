089ebc44a39a9bf5b1a6e05ccd86c7f2db4f8702
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-18 22:28:59 +0530

    2.0

---
+++ models/Message.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
