7e23b85fbdfc41766a4e674baf8c3c54b8afda1a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-18 21:37:02 +0530

    2.0

--- public/js/sidebar-toggle.js
+++ public/js/sidebar-toggle.js
@@ -8,6 +8,8 @@ toggleButton.addEventListener('click', () => {
 });
 
 
-document.querySelector('.sidebar-toggle').addEventListener('click', function() {
-    document.querySelector('.sidebar').classList.toggle('hidden');
+
+document.querySelector('.sidebar-toggle').addEventListener('click', () => {
+    document.querySelector('.sidebar').classList.toggle('collapsed');
+    document.querySelector('.main-content').classList.toggle('expanded');
 });
