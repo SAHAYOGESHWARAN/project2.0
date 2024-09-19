d3456cce1e0b4a01c71fb91c3347dbd8567e25bb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-19 20:16:21 +0530

    2.0

---
+++ config/db.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const connectDB = require('./config/db'); 
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('MongoDB Connection Established');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
