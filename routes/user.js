496c2c98ee9224f5b5437ce07781088a8607cf75
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-20 10:05:40 +0530

    2.0

--- routes/user.js
+++ routes/user.js
@@ -39,14 +39,5 @@ router.post('/add', async (req, res) => {
 });
 
 
-// GET /users - Get all users
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch users' });
-    }
-});
 
 module.exports = router;
