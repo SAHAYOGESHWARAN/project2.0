0a4cbcce56bbdb2bed8cdd52faf6253307c72565
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-21 10:34:44 +0530

    2.0

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,16 @@
 
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product'); // Import your Product model
+const Product = require('../models/Product'); // Make sure to import your Product model
 
 // Route to get all products
 router.get('/products', async (req, res) => {
     try {
-        const products = await Product.find(); // Query to get all products
-        res.render('products', { products }); // Passing 'products' to the view
+        const products = await Product.find(); // Fetch products from the database
+        res.render('products', { products }); // Pass the products to the view
     } catch (error) {
-        res.status(500).send('Server Error');
+        console.error(error); // Log the error for debugging
+        res.status(500).send('Server Error'); // Send a 500 response if an error occurs
     }
 });
 
